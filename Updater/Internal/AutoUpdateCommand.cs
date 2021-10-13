63b52b8cb814532101228650da8ca27a2bfdc4cb
Wydział Wewnętrzny <gamer@mistaken.pl>
2021-10-13 08:52:32 +0200

    force rejoin

--- Updater/Internal/AutoUpdateCommand.cs
+++ Updater/Internal/AutoUpdateCommand.cs
@@ -10,6 +10,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CommandSystem;
+using Exiled.API.Features;
 using Exiled.API.Interfaces;
 using Mistaken.Updater.Config;
 
@@ -45,9 +46,17 @@ namespace Mistaken.Updater.Internal
             if (pluginName == "*")
             {
                 if (AutoUpdater.Instance.DoAutoUpdates())
-                    Exiled.API.Features.Server.Restart();
-
-                return true;
+                {
+                    Server.Host.ReferenceHub.playerStats.RpcRoundrestart((float)GameCore.ConfigFile.ServerConfig.GetInt("full_restart_rejoin_time", 25), true);
+                    MEC.Timing.CallDelayed(1, () => Server.Restart());
+                    response = "Restarting";
+                    return true;
+                }
+                else
+                {
+                    response = "No need to restart";
+                    return false;
+                }
             }
 
             var plugin = Exiled.Loader.Loader.Plugins.Where(x => x.Config is IAutoUpdatableConfig).Select(x => x as IPlugin<IAutoUpdatableConfig>).FirstOrDefault(x => x.Name.ToLower() == pluginName || x.Prefix.ToLower() == pluginName);
@@ -59,7 +68,10 @@ namespace Mistaken.Updater.Internal
             }
 
             if (AutoUpdater.Instance.DoAutoUpdate(plugin, false))
-                Exiled.API.Features.Server.Restart();
+            {
+                Server.Host.ReferenceHub.playerStats.RpcRoundrestart((float)GameCore.ConfigFile.ServerConfig.GetInt("full_restart_rejoin_time", 25), true);
+                MEC.Timing.CallDelayed(1, () => Server.Restart());
+            }
 
             response = $"Updated {plugin.Author}.{plugin.Name}";
             return true;
